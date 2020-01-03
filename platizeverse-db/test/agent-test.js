ba41c42dce07cc1c308eb1ded639ea5ddb873871
ramiro <rhuayller@agetic.gob.bo>
2020-01-03 18:07:57 -0400

    avances

--- platizeverse-db/test/agent-test.js
+++ platizeverse-db/test/agent-test.js
@@ -1,9 +1,32 @@
 const test = require('ava');
+const sinon = require('sinon');
+const proxyquire = require('proxyquire');
 
+let config = {
+    logging: function() {}
+}
 
+let MetricStub = {
+    belongsTo: sinon.spy()
+};
 
+let AgentStub = null;
+let db = sinon.spy();
 
+test.beforeEach(async () => {
+    AgentStub: {
+        hasMany = function() {
+
+        }
+    }
+    // const setupDatabase = require('./'); // en vez de esto usar proxyquire
+    const setupDatabase = proxyquire('../', {
+        './models/agent': () => AgentStub,
+        './models/metric': () => MetricStub
+    });
+    db = await setupDatabase(config);
+})
 
 test('Agent', t => {
-    t.pass();
+    t.truthy(db.Agent, 'Agent servicde should exist');
 })
---
+++ platizeverse-db/test/fixtures/agent.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const agent = {
+    id: 1,
+    uuid: 'sadf',
+    name: 'my name',
+    username: 'sdaffds',
+    hostname: 'localhost',
+    pid: 32,
+    connected: true,
+    createdAt: new Date(),
+    updatedAt: new Date()
+};
+
+const agents = [
+    agent,
+    
+];
